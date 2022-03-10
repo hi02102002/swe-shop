2232a928a5c1250e39359bd483e6753f7ba5c663
hi02102002 <2051120240@ut.edu.vn>
2022-03-10 15:20:14 +0700

    remove unuse

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { Middleware } from '@reduxjs/toolkit';
 
 export const middleware: Middleware = (storeApi) => (next) => (action) => {
    if (action.type === 'auth/login/fulfilled') {
-      const currentUser = storeApi.getState().auth.currentUser;
+      // const currentUser = storeApi.getState().auth.currentUser;
       //   storeApi.dispatch(getAllCarts(currentUser.id));
    }
    return next(action);
